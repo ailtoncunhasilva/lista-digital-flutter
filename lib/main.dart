4b9759f817b22b3ce26f0130619f4653e77cf5e1
ailtoncunhasilva <ailtoncunhasilva@hotmail.com>
2020-09-23 10:42:26 -0300

    created functions2

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,9 @@
+import 'dart:convert';
+
 import 'package:flutter/material.dart';
+import 'package:path_provider/path_provider.dart';
+import 'dart:async';
+import 'dart:io';
 
 void main() => runApp(MyApp());
 
@@ -22,14 +27,34 @@ class MyHomePage extends StatefulWidget {
 }
 
 class _MyHomePageState extends State<MyHomePage> {
+  List _toDoList = [];
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: Text('Lista Digital'),
-        centerTitle: true
-      ),
+      appBar: AppBar(title: Text('Lista Digital'), centerTitle: true),
     );
   }
-}
 
+  Future<File> _getFile() async {
+    final directory = await getApplicationDocumentsDirectory();
+    return File("${directory.path}/data.json");
+  }
+
+  Future<File> _saveData() async {
+    String data = json.encode(_toDoList);
+
+    final file = await _getFile();
+    return file.writeAsString(data);
+  }
+
+  Future<String> _readData() async{
+    try{
+      final file = await _getFile();
+
+      return file.readAsString();
+    }catch(e){
+      return null;
+    }
+  }
+}
